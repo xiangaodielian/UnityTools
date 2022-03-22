09094db3b12072734593b49c3f5157828af5cf25
吴文华 <75501737@qq.com>
2022-03-22 19:28:38 +0800

    添加一个可以拖动的无限循环列表

---
+++ Assets/Scripts/Extend/EmptyImage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+
+public class EmptyImage : MaskableGraphic
+{
+    protected EmptyImage()
+    {
+        useLegacyMeshGeneration = false;
+    }
+    protected override void OnPopulateMesh(VertexHelper toFill)
+    {
+        toFill.Clear();
+    }
+}
